bc6773fda36b10bd12c29468cada3dc3297c3196
Dennis O’Reilly <doreilly@umich.edu>
2011-12-20 15:16:25 -0500

    Converted the field rendering into individual templates for modularity. Additionally added a formset rendering feature.

--- bootstrapform/templatetags/bootstrap.py
+++ bootstrapform/templatetags/bootstrap.py
@@ -11,16 +11,28 @@ def bootstrap(form):
     return template.render(context)
 
 @register.filter
-def bootstrapfield(field):
-    template = get_template("bootstrapform/field.html")
-    context = Context({'field': field})
+def bootstrapformset(formset):
+    template = get_template("bootstrapform/formset.html")
+    context = Context({'formset': formset})
     return template.render(context)
 
-@register.filter
-def is_checkbox(field):
-    return field.field.widget.__class__.__name__.lower() == "checkboxinput"
+field_templates = {
+    'radioselect': 'multiple_input',
+    'checkboxselectmultiple': 'multiple_input',
+    'checkboxinput': 'checkbox_input',
+}
+
+def get_field_type(field):
+    return field.field.widget.__class__.__name__.lower()
+
+def get_field_template(field):
+    field_type = get_field_type(field)
+    template_name = field_templates.get(field_type, 'generic')
+    return get_template('bootstrapform/fields/%s.html' % template_name)
 
 @register.filter
-def is_radio(field):
-    return field.field.widget.__class__.__name__.lower() == 'radioselect'
+def bootstrapfield(field):
+    template = get_field_template(field)
+    context = Context({'field': field})
+    return template.render(context)
 
