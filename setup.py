926c2bd7f507e45462d45f14f2c55e7fa75d6a9b
Roman Dolgiy <roman@bravetstudio.com>
2011-12-25 00:03:53 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,8 @@ from bootstrapform.meta import VERSION
 
 setup(name='django-bootstrap-form',
     author='tzangms', author_email='tzangms@gmail.com',
-    url='https://github.com/tzangms/django-bootstrap-form',
+    url='https://github.com/t0ster/django-bootstrap-form',
     version=str(VERSION),
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['django'],
-    )
-
+)
